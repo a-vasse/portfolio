300dda1f773160e2e88e8ff4e8738ed5c2e0ede0
Alex Vasse <alexandervasse@gmail.com>
2022-12-17 17:25:26 +0900

    added menu drop down reactive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
+  const[Toggle, showMenu] = useState(false);
   return (
-    <header className="head">
+    <header className="header">
       <nav className="nav container">
         <a href="index.html" className="nav__logo">Vasse</a>
-        <div className="nav__menu">
+
+        <div className={Toggle ? "nav__menu show-menu" : "nav__menu" }>
           <ul className="nav__list grid">
 
             <li className="nav__item">
-              <a href="#home" className="nav__link">
+              <a href="#home" className="nav__link active-link">
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>
             </li>
@@ -44,8 +46,11 @@ const Header = () => {
                 <i className="uil uil-message nav__icon"></i> Contact
               </a>
             </li>
-
           </ul>
+          <i class="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
+        </div>
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
+          <i class="uil uil-apps"></i>
         </div>
       </nav>
     </header>
