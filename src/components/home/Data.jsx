bb8c7300a0f557d3f84c610aab5ca0f07f5c2d71
Alex Vasse <alexandervasse@gmail.com>
2022-12-18 14:18:06 +0900

    added icons

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -8,9 +8,27 @@ const Data = () => {
         </h1>
         <h3 className="home-subtitle">Full Stack Web Developer</h3>
         <p className="home-description text-dark">Full stack web developer residing just outside Tokyo. Ruby on Rails | Javascript | HTML/CSS</p>
-        <a href="#contact" className="btn btn-primary">
-          Contact Me <i class="fa-regular fa-comments"></i>
-        </a>
+        <div class="d-flex gap-3">
+          <div class="mt-2">
+          <a href="#contact" className="btn btn-primary p-3">
+            Contact Me <i class="fa-regular fa-comments"></i>
+          </a>
+          </div>
+          <ul class="icon-wrapper">
+          <a href="http://www.linkedin.com/in/alexandervasse" rel="noopener noreferrer" target="_blank">
+            <li class="icon linkedin"><span class="tooltip">LinkedIn</span>
+            <i class="fab fa-linkedin"></i>
+          </li></a>
+          <a href="http://www.github.com/a-vasse" rel="noopener noreferrer" target="_blank">
+            <li class="icon github"><span class="tooltip">Github</span>
+            <i class="fab fa-github"></i>
+          </li></a>
+          <a href="http://www.instagram.com/usagishashin" rel="noopener noreferrer" target="_blank">
+            <li class="icon instagram"><span class="tooltip">Instagram</span>
+            <i class="fab fa-instagram"></i>
+          </li></a>
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import "./home.css";
-import Social from "./Social";
 import Data from "./Data";
 
 const Home = () => {
@@ -10,12 +9,12 @@ const Home = () => {
       <section className="home section" id="home">
         <div className="home-container container grid">
           <div className="home-content grid">
-            <Social />
+
 
             <div className="home-img"></div>
-
             <Data />
           </div>
+
         </div>
       </section>
     </div>
