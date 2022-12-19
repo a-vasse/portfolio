e9f0a58bdc2427db291179c006142c1e509f6e9e
Alex Vasse <alexandervasse@gmail.com>
2022-12-19 21:45:29 +0900

    all four modals working

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./services.css"
 
 const Services = () => {
+  const [toggleState, setToggleState] = useState(0);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  }
   return (
     <section className="skills section" id="skills">
       <h2 className="section-title">Skills</h2>
@@ -14,11 +19,12 @@ const Services = () => {
             <h3 className="service-title">Frontend<br />Development</h3>
           </div>
 
-          <span className="services-button">View More{" "}<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
+          <span className="services-button" onClick={() => toggleTab(1)}>
+            View More<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
 
-          <div className="services-modal">
+          <div className={toggleState === 1 ? "services-modal active-modal" : "services-modal"}>
             <div className="services-modal-content">
-              <i className="fa-solid fa-xmark"></i>
+              <i onClick={() => toggleTab(0)} className="fa-solid fa-xmark services-modal-close"></i>
               <h3 className="services-modal-title">Title</h3>
               <p className="services-modal-description">Description</p>
               <ul className="services-modal-services grid">
@@ -52,11 +58,12 @@ const Services = () => {
             <h3 className="service-title">Backend<br />Development</h3>
           </div>
 
-          <span className="services-button">View More{" "}<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
+          <span className="services-button" onClick={() => toggleTab(2)}>
+            View More<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
 
-          <div className="services-modal">
+            <div className={toggleState === 2 ? "services-modal active-modal" : "services-modal"}>
             <div className="services-modal-content">
-              <i className="fa-solid fa-xmark"></i>
+              <i onClick={() => toggleTab(0)} className="fa-solid fa-xmark services-modal-close"></i>
               <h3 className="services-modal-title">Title</h3>
               <p className="services-modal-description">Description</p>
               <ul className="services-modal-services grid">
@@ -90,11 +97,12 @@ const Services = () => {
             <h3 className="service-title">Tools<br />I Use</h3>
           </div>
 
-          <span className="services-button">View More{" "}<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
+          <span className="services-button" onClick={() => toggleTab(3)}>
+            View More<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
 
-          <div className="services-modal">
+            <div className={toggleState === 3 ? "services-modal active-modal" : "services-modal"}>
             <div className="services-modal-content">
-              <i className="fa-solid fa-xmark"></i>
+              <i onClick={() => toggleTab(0)} className="fa-solid fa-xmark services-modal-close"></i>
               <h3 className="services-modal-title">Title</h3>
               <p className="services-modal-description">Description</p>
               <ul className="services-modal-services grid">
@@ -128,11 +136,12 @@ const Services = () => {
             <h3 className="service-title">Basic<br />Knowledge</h3>
           </div>
 
-          <span className="services-button">View More{" "}<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
+          <span className="services-button" onClick={() => toggleTab(4)}>
+            View More<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
 
-          <div className="services-modal">
+            <div className={toggleState === 4 ? "services-modal active-modal" : "services-modal"}>
             <div className="services-modal-content">
-              <i className="fa-solid fa-xmark"></i>
+              <i onClick={() => toggleTab(0)} className="fa-solid fa-xmark services-modal-close"></i>
               <h3 className="services-modal-title">Title</h3>
               <p className="services-modal-description">Description</p>
               <ul className="services-modal-services grid">
