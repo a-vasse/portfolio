757684611213ca0514aaa56fd005f2e54190ba0c
Alex Vasse <alexandervasse@gmail.com>
2022-12-20 02:52:00 +0900

    more additions to skills. Go. To. Sleep.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -29,14 +29,14 @@ const Header = () => {
             </li>
 
             <li className="nav-item">
-              <a href="#services" className="nav-link">
-                <i className="fa-solid fa-toolbox nav-icon"></i>Services
+              <a href="#portfolio" className="nav-link">
+                <i className="fa-solid fa-photo-film nav-icon"></i>Portfolio
               </a>
             </li>
 
             <li className="nav-item">
-              <a href="#portfolio" className="nav-link">
-                <i className="fa-solid fa-photo-film nav-icon"></i>Portfolio
+              <a href="#history" className="nav-link">
+                <i className="fa-solid fa-briefcase nav-icon"></i>History
               </a>
             </li>
 
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import DetailSkills from './detailedskills/DetailSkills';
 import BasicSkills from './basicskills/BasicSkills';
 import "./skills.css";
 
 const Skills = () => {
+  const [toggleState, setToggleState] = useState(1);
   return (
-    <section className="basicskills section" id="basicskills">
+    <section className="skills section" id="basicskills">
       <h2 className="section-title">Skills</h2>
       <span className="section-subtitle">My Technical Abilities</span>
       <div className="skills-container container grid">
-        <BasicSkills />
-        <DetailSkills />
+
+        <div className="history-tabs">
+          <div className={toggleState === 1 ? "history-button history-active button--flex"
+            : "history-button button--flex"} onClick={() => setToggleState(1)}>
+            <i className="fa-solid fa-briefcase history-icon"></i>{" "}Basic
+          </div>
+          <div className={toggleState === 2 ? "history-button history-active button--flex"
+            : "history-button button--flex"} onClick={() => setToggleState(2)}>
+            <i className="fa-solid fa-graduation-cap history-icon"></i>{" "}Detailed
+          </div>
+        </div>
+
+        <div className="history-sections">
+          <div className={toggleState === 1 ? "history-content history-content-active"
+              : "history-content"}>
+            <BasicSkills />
+          </div>
+          <div className={toggleState === 2 ? "history-content history-content-active"
+              : "history-content"}>
+            <DetailSkills />
+          </div>
+        </div>
       </div>
     </section>
   );
