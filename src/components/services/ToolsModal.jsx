5277eab15c0f5f8883d4a5b3c155fc92f55bc6b5
Alex Vasse <alexandervasse@gmail.com>
2022-12-19 22:16:52 +0900

    refactored modal code

--- src/components/services/ToolsModal.jsx
+++ src/components/services/ToolsModal.jsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 
 const ToolsModal = () => {
   const [toggleState, setToggleState] = useState(0);
-
-  const toggleTab = (index) => {
-    setToggleState(index);
-  }
   return (
     <div className="service-content">
       <div>
@@ -13,12 +9,12 @@ const ToolsModal = () => {
         <h3 className="service-title">Tools<br />I Use</h3>
       </div>
 
-      <span className="services-button" onClick={() => toggleTab(1)}>
+      <span className="services-button" onClick={() => setToggleState(1)}>
         View More<i className="fa-solid fa-arrow-right services-button-icon"></i></span>
 
-        <div className={toggleState === 1 ? "services-modal active-modal" : "services-modal"}>
+      <div className={toggleState === 1 ? "services-modal active-modal" : "services-modal"}>
         <div className="services-modal-content">
-          <i onClick={() => toggleTab(0)} className="fa-solid fa-xmark services-modal-close"></i>
+          <i onClick={() => setToggleState(0)} className="fa-solid fa-xmark services-modal-close"></i>
           <h3 className="services-modal-title">Tools I Use</h3>
           <p className="services-modal-description">Description</p>
           <ul className="services-modal-services grid">
