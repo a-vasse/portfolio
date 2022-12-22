3d0695e3232d3d1f0f7938bacee20ace43800be3
Alex Vasse <alexandervasse@gmail.com>
2022-12-22 14:51:11 +0900

    version 1.0 complete

--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -1,6 +1,6 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
+import Work1 from "../../assets/projects/SubstrackedThumb.png";
+import Work2 from "../../assets/projects/RentARetroThumb.png";
+import Work3 from "../../assets/projects/FitbookThumb.png";
 
 export const projectsData = [
   {
@@ -8,18 +8,24 @@ export const projectsData = [
     image: Work1,
     title: "Substracked",
     category: "team",
+    url: "http://www.substracked.com",
+    gh: "https://github.com/a-vasse/substracked"
   },
   {
     id: 2,
     image: Work2,
     title: "Rent a Retro",
     category: "team",
+    url: "https://rent-a-retro.herokuapp.com/",
+    gh: "https://github.com/a-vasse/Rent-a-retro"
   },
   {
     id: 3,
     image: Work3,
     title: "Fitbook",
     category: "individual",
+    url: "https://fitbook-av.herokuapp.com",
+    gh: "https://github.com/a-vasse/rails-fitbook"
   },
 ];
 
--- src/components/projects/ProjectItems.jsx
+++ src/components/projects/ProjectItems.jsx
@@ -5,9 +5,14 @@ const ProjectItems = ({item}) => {
     <div className="project-card" key={item.id}>
       <img src={item.image} alt="" className="project-img"/>
       <h3 className="project-title">{item.title}</h3>
-      <a href="#" className="project-button">
-        Demo <i className="fa-solid fa-arrow-right project-button-icon"></i>
-      </a>
+      <div className="project-links">
+        <a href={item.url} className="project-button">
+          Demo <i className="fa-solid fa-arrow-right project-button-icon"></i>
+        </a>
+        <a href={item.gh} className="project-button">
+          Source <i className="fa-solid fa-arrow-right project-button-icon"></i>
+        </a>
+      </div>
     </div>
   );
 };
