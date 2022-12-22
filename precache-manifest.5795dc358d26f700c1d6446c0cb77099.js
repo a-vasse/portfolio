6993f7cd4f66104474eb64d964f3670bddff86fa
Alex Vasse <alexandervasse@gmail.com>
2022-12-22 15:01:28 +0900

    Updates

---
+++ precache-manifest.5795dc358d26f700c1d6446c0cb77099.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "13423de9a4bdc31ac2d4",
+    "url": "/portfolio/static/css/main.cb9fd0c2.chunk.css"
+  },
+  {
+    "revision": "13423de9a4bdc31ac2d4",
+    "url": "/portfolio/static/js/main.b0343bba.chunk.js"
+  },
+  {
+    "revision": "04ee6062cb738088f203",
+    "url": "/portfolio/static/js/runtime~main.e7aeffe1.js"
+  },
+  {
+    "revision": "aa30610acaee7b71fd33",
+    "url": "/portfolio/static/css/2.ffb23492.chunk.css"
+  },
+  {
+    "revision": "aa30610acaee7b71fd33",
+    "url": "/portfolio/static/js/2.cf64e8ba.chunk.js"
+  },
+  {
+    "revision": "307b298eaad73106f9a9",
+    "url": "/portfolio/static/js/3.b0609f3e.chunk.js"
+  },
+  {
+    "revision": "e3806983bf2a842549dcea70916ae9e0",
+    "url": "/portfolio/static/media/about.e3806983.jpg"
+  },
+  {
+    "revision": "69583c7147738729af2556b948594891",
+    "url": "/portfolio/static/media/resume.69583c71.pdf"
+  },
+  {
+    "revision": "2ddc44cc9b28c4a194d22e03a5db05dc",
+    "url": "/portfolio/static/media/photo01.2ddc44cc.png"
+  },
+  {
+    "revision": "ee908b82b4db1f82f9588be21c522041",
+    "url": "/portfolio/static/media/photo02.ee908b82.png"
+  },
+  {
+    "revision": "4b84057bd408c35e52d31b17b6ab84d7",
+    "url": "/portfolio/static/media/photo03.4b84057b.png"
+  },
+  {
+    "revision": "342e2153699335d591e29ae9118ede5a",
+    "url": "/portfolio/static/media/photo04.342e2153.png"
+  },
+  {
+    "revision": "1dfc9c0080cf7a7f87eef0a8880d01b7",
+    "url": "/portfolio/static/media/photo05.1dfc9c00.png"
+  },
+  {
+    "revision": "9b9e572a641071c0c95d555fe68e5bbe",
+    "url": "/portfolio/static/media/photo06.9b9e572a.png"
+  },
+  {
+    "revision": "8acc3c80cd707f952f91564951975152",
+    "url": "/portfolio/static/media/photo07.8acc3c80.png"
+  },
+  {
+    "revision": "8b1d9760a041c883f03fa9aad7d45817",
+    "url": "/portfolio/static/media/photo08.8b1d9760.png"
+  },
+  {
+    "revision": "1d269360b641663763d92d4abe9a4995",
+    "url": "/portfolio/static/media/photo09.1d269360.png"
+  },
+  {
+    "revision": "e4acca56430c6ade361a34916a7bccca",
+    "url": "/portfolio/static/media/photo10.e4acca56.png"
+  },
+  {
+    "revision": "5d83b2e8204e82525eea660b910d7aad",
+    "url": "/portfolio/static/media/photo11.5d83b2e8.png"
+  },
+  {
+    "revision": "dcebbf0e5fbdbe2cde5e79b7281a2dea",
+    "url": "/portfolio/static/media/SubstrackedThumb.dcebbf0e.png"
+  },
+  {
+    "revision": "5a973f3d692387616505fd45ec721a9a",
+    "url": "/portfolio/static/media/RentARetroThumb.5a973f3d.png"
+  },
+  {
+    "revision": "6d1d9c8c02657002ca7f5e083ddb84df",
+    "url": "/portfolio/static/media/FitbookThumb.6d1d9c8c.png"
+  },
+  {
+    "revision": "8f4193f09446831ced66a2d54da07474",
+    "url": "/portfolio/static/media/profile.8f4193f0.jpg"
+  },
+  {
+    "revision": "a6cf2da72637996e97e99649a379b2e3",
+    "url": "/portfolio/index.html"
+  }
+];
