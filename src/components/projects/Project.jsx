7f1a0f3e4c7bb4e85e3b38d7b24ae216a9126f5e
Alex Vasse <alexandervasse@gmail.com>
2022-12-22 14:10:10 +0900

    progress made on projects section

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,21 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { projectsData } from './ProjectData';
 import { projectsNav } from './ProjectData';
 import ProjectItems from './ProjectItems';
 
 const Project = () => {
+  const [item, setItem] = useState({name: "all"});
+  const [projects, setProjects] = useState([]);
+  const [active, setActive] = useState(0);
+
+  useEffect(() => {
+    if(item.name === "all") {
+      setProjects(projectsData);
+    }
+    else {
+      const newProjects = projectsData.filter((project) => {
+        return project.category === item.name;
+      });
+      setProjects(newProjects);
+    }
+  }, [item]);
+
+  const handleClick = (event, index) => {
+    setItem({name: event.target.textContent });
+  };
+
   return (
     <div>
       <div className="projects-filters">
         {projectsNav.map((item, index) => {
           return (
-            <span className="projects-item" key={index} >{item.name}</span>
-          )
+            <span onClick={(event) => {
+              handleClick(event, index);
+            }}
+
+            className="projects-item" key={index} >{item.name}</span>
+          );
         })}
       </div>
 
       <div className="projects-container container grid">
-        {projectsData.map((item) => {
+        {projects.map((item) => {
           return <ProjectItems item={item} key={item.id}/>
         })}
       </div>
--- src/components/projects/ProjectData.jsx
+++ src/components/projects/ProjectData.jsx
@@ -1,8 +1,6 @@
 import Work1 from "../../assets/work1.jpg";
 import Work2 from "../../assets/work2.jpg";
 import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
   {
