56b334472d3d2b23cb0514536f8473332647501f
Alex Vasse <alexandervasse@gmail.com>
2023-01-29 22:53:35 +0900

    edits to intro

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -6,8 +6,8 @@ const Intro = () => {
       <svg width="120%" height="40%">
         <text x="45%" y="60%" textAnchor="middle">Alexander Vasse</text>
       </svg>
-      <h3 className="home-subtitle">Full Stack Web Developer</h3>
-      <p className="home-description">Developer residing just outside Tokyo. Ruby on Rails | Javascript | HTML/CSS</p>
+      <h3 className="home-subtitle">Web Developer</h3>
+      <p className="home-description">Developer based in Tokyo. Ruby on Rails | React.js | Javascript | HTML/CSS</p>
       <a href="#contact" className="button button--flex">Contact Me<i className="fa-regular fa-comments"></i></a>
       <ul className="icon-wrapper">
         <a href="http://www.linkedin.com/in/alexandervasse" rel="noopener noreferrer" target="_blank">
